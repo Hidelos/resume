3c84682b5c6b2b4509e79fd6242518a0faa9b841
Hidelos <67259931+Hidelos@users.noreply.github.com>
2020-11-24 10:01:58 +0900

    Update common.js

--- Mobile/js/common.js
+++ Mobile/js/common.js
@@ -42,7 +42,7 @@ $(document).ready(function(){
 
     var didScroll;
     var lsTop = 0;
-    var delta = 40;
+    var delta = 20;
 
     $(window).on('load scroll', function(){
         didScroll = true;
@@ -91,7 +91,7 @@ $(document).ready(function(){
             }
             
             
-            if (sTop <= 40) {
+            if (sTop <= 20) {
                 $Topview.removeClass('fixed').removeClass('up');
             $header.removeClass('fixed').removeClass('up');
             $subWrap.removeClass('fixed');
